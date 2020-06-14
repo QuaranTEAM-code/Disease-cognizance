a087803b4a68e3503652314c2e952841c81d76d6
QuaranTEAM-code <63850979+QuaranTEAM-code@users.noreply.github.com>
2020-06-14 19:18:57 +0530

    About Covid - Anrup

---
+++ ABOUT_COVID2/src/about_covid/ABOUT_COVID.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package about_covid;
+
+/**
+ *
+ * @author Anrup Sharma
+ */
+public class ABOUT_COVID {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
